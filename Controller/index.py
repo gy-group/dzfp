ee290a6d9395ddcd8c3ba9a99b4da19d3bd398c1
lyy <cookiecoke@163.com>
2017-04-26 14:03:19 +0800

    init project

---
+++ Controller/index.py
@@ -0,0 +1,14 @@
+from flask import render_template;
+from flask import session,request,abort,Blueprint;
+import os;
+
+
+main =  Blueprint('main',__name__,template_folder='templates')
+
+@main.route('/')
+def index():
+    return render_template("index.html")
+
+@main.route('/home')
+def home():
+    return render_template("home.html")
---
+++ _init_.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from Controller.index import main;
+app = Flask(__name__)
+
+app.register_blueprint(main)
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+app.run(debug=True)
